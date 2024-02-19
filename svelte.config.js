b38ab5d865b23dec3fbeba628e5528a81da07c59
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-02-19 16:54:38 +0300

    Add skeleton app

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-node'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+  kit: {
+    adapter: adapter({
+      precompress: true
+    })
+  },
+  preprocess: vitePreprocess()
+}
